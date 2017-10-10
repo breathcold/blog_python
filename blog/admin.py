ed7f32af101c18d5e51d1b412d629e34b68f6070
13027196096 <13027196096@163.com>
2017-10-10 14:10:47 +0800

    收工

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from blog.models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'create_time')
+    list_filter = ('create_time', )
+    #tuple只有一个元素的时候需要加上一个逗号
+
+admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Article(models.Model):
     title = models.CharField(max_length=32, default='Title')
     content = models.TextField(null=True)
+    create_time = models.TimeField(null=True)
     
     def __str__(self):
         return self.title
